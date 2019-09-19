f66488c7f4333ecbe984ceec9d7c2bc0e28dd9f7
Rodrigo Candido <rodrigocmo.22@gmail.com>
2019-09-19 16:58:46 -0300

    Login

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,15 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+
 public class Controller {
+
+    @FXML
+    private void handleButtonAction(){
+        System.exit(0);
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,19 +4,27 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setScene(new Scene(root));
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.show();
+
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
