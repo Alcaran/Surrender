a39ccc161e087283470bd4fc333a0a8da27dcbdd
Rodrigo Candido <rodrigocmo.22@gmail.com>
2019-10-31 21:00:24 -0300

    Drawer working

--- src/sample/MenuController.java
+++ src/sample/MenuController.java
@@ -1,14 +1,19 @@
 package sample;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDrawer;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -23,9 +28,41 @@ public class MenuController implements Initializable {
     JFXComboBox<String> comboBox;
     @FXML
     private BorderPane bp;
+    @FXML
+    private JFXDrawer drwMenu;
 
     public void initialize(URL url, ResourceBundle rb){
+        try {
+            VBox box = FXMLLoader.load(getClass().getResource("Drawer.fxml"));
+            drwMenu.setSidePane(box);
+            for (Node node : box.getChildren()){
+                node.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) ->{
+                    switch (node.getId())
+                    {
+                        case "btnBack" :
+                            drawCloseButton();
+                            break;
+                        case "btnLogin" :
+                            Stage s = new Stage();
+                            try {
+                                Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+                                s.setScene(new Scene(root));
+                                s.initStyle(StageStyle.TRANSPARENT);
+                                s.show();
+                                Stage stage  = (Stage) bp.getScene().getWindow();
+                                stage.close();
+                            }
+                            catch (IOException ex) {
+                                ex.printStackTrace();
+                            }
+                            break;
+                    }
+                });
+            }
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         comboBox.getItems().add("BR");
         comboBox.getItems().add("LAS");
         comboBox.getItems().add("KR");
@@ -36,12 +73,11 @@ public class MenuController implements Initializable {
 
     @FXML
     private void keyPressed(KeyEvent k) throws IOException{
-         if(k.getCode() == KeyCode.ENTER) {
+        if(k.getCode() == KeyCode.ENTER) {
              Stage s = new Stage();
              Parent root = FXMLLoader.load(getClass().getResource("Profile.fxml"));
              s.setScene(new Scene(root));
              s.initStyle(StageStyle.TRANSPARENT);
-
              s.show();
 
              Stage stage  = (Stage) bp.getScene().getWindow();
@@ -50,13 +86,25 @@ public class MenuController implements Initializable {
     }
 
     @FXML
-    private void handleButtonAction() {
+    private void handleButtonAction(){
         System.exit(0);
     }
 
     @FXML
-    private void minimizeButtonAction() throws IOException {
+    private void minimizeButtonAction(){
         Stage stage  = (Stage) bp.getScene().getWindow();
         stage.setIconified(true);
+    }
+    @FXML
+    private void drawButton() {
+        drwMenu.open();
+        drwMenu.setPrefWidth(200.00);
+        drwMenu.setVisible(true);
+        drwMenu.setDisable(false);
+    }
+    @FXML
+    private void drawCloseButton() {
+        drwMenu.close();
+        drwMenu.setDisable(true);
     }
 }
